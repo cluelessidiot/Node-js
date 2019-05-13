5117cd4a4326a81f8dc05df8cfe59fa4422a7fad
AATHIL.TA <33000688+cluelessidiot@users.noreply.github.com>
2019-05-13 13:18:05 +0530

    Update io.js

--- part1/io.js
+++ part1/io.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
-var buff = fs.readFileSync(process.argv[2]);
-var strbuff=buff.toString();
+var buff = fs.readFileSync(process.argv[2],'utf8');
+//var strbuff=buff.toString();
 //console.log(strbuff);
 var ct=0;
-for(i=0;i<strbuff.length;i++)
+for(i=0;i<buff.length;i++)
     {
-        if(strbuff[i]=='\n')
+        if(buff[i]=='\n')
             ct++;
     }
 //ct++;
